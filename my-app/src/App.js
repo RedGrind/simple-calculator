70775829ac49eb58a8759aba91de2522c2f23ab0
RedGrind <ozorjikj@gmail.com>
2022-05-28 23:25:09 -0500

    Little changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Hello world!</p>
+        <p> Time for Calculations!</p>
       </header>
     </div>
   );
